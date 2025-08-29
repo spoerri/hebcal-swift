9177e6dac6c503475c45a0dd9ab85db2a2257cd6
spoerri <spoerri@users.noreply.github.com>
2025-08-28 22:11:07 -0400

    Switch the suncalc dependency, because the original is no longer on github

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "Hebcal",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v10_14), .iOS(.v11), .watchOS(.v5)
+        .macOS(.v10_14), .iOS(.v16), .watchOS(.v5)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,14 +18,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/stefanspringer1/SunCalcSwift.git", from: "1.0.0"),
+        .package(url: "https://github.com/Timac/SunCalc.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Hebcal",
-            dependencies: ["SunCalcSwift"]),
+            dependencies: ["SunCalc"]),
         .testTarget(
             name: "HebcalTests",
             dependencies: ["Hebcal"]),
