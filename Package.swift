f630b86f3b111e868e77d372631640fc88b14ec1
spoerri <spoerri@users.noreply.github.com>
2025-08-28 22:15:54 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Hebcal",
-            dependencies: ["SunCalcSwift"]),
+            dependencies: ["SunCalc"]),
         .testTarget(
             name: "HebcalTests",
             dependencies: ["Hebcal"]),
